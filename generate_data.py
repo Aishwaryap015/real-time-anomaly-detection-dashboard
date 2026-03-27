c531be260011b2545d3b718d3d7de3b82d396d16
Aishwaryap015 <pdarshniaishwarya@gmail.com>
2026-03-27 18:59:54 +0530

    Removed heavy dependencies and optimized for deployment

--- generate_data.py
+++ generate_data.py
@@ -8,16 +8,36 @@ def generate_data():
         writer.writerow(["timestamp", "value"])
 
         while True:
-            value = random.randint(10, 100)
+    try:
+        df = detect_anomalies()
 
-            # Inject anomaly
-            if random.random() < 0.05:
-                value = random.randint(150, 300)
+        if "anomaly" in df.columns:
+            anomalies = df[df["anomaly"] == -1]
 
-            writer.writerow([time.time(), value])
-            file.flush()
+            drift_detected, p_value = detect_drift()
 
-            time.sleep(1)
+            with placeholder.container():
+
+                col1, col2 = st.columns(2)
+                col1.metric("Total Data Points", len(df))
+                col2.metric("Anomalies Detected", len(anomalies))
+
+                if not anomalies.empty:
+                    st.error(f"🚨 {len(anomalies)} anomalies detected!")
+
+                if drift_detected:
+                    st.warning(f"⚠️ Data Drift Detected!")
+
+                st.line_chart(df["value"])
+
+                st.subheader("Detected Anomalies")
+                st.write(anomalies)
+
+        time.sleep(2)   # ✅ aligned with try
+
+    except Exception as e:
+        st.write("Waiting for data...")
+        time.sleep(2)
 
 if __name__ == "__main__":
     generate_data()
